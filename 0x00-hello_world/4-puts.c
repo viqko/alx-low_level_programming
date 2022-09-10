d2bff94341ec9ff73318f5f57c13693428d1680d
TETTEH VICTOR TETTEY <tvictortetteh@gmail.com>
2022-09-10 16:28:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
