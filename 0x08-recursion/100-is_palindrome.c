a8943c2afcb2b1b29f0bd143a151879b1c7d7d70
Victor Tettey TETTEH <tvictortetteh@gmail.com>
2022-09-28 20:14:00 +0100

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * compare - compare head and tail indices for match
+ * @head: index starting from left of string
+ * @tail: index starting from right of string, moving backwards
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int compare(char *head, char *tail)
+{
+	if (head >= tail)
+		return (1);
+	if (*head == *tail)
+		return (compare(head + 1, tail - 1));
+	return (0);
+}
+
+/**
+ * _strlen - find length of string to access last index
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + (_strlen(s)));
+}
+
+/**
+ * is_palindrome - check if palindrome
+ * @s: string to check
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen(s);
+
+	return (compare(s, (s + len - 1)));
+}
