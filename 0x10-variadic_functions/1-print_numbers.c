8bbc86b709bb9ccea7665425a8529d386e33fc69
Victor Tettey TETTEH <tvictortetteh@gmail.com>
2022-10-13 18:14:31 +0100

    Create 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print varying amount of numbers
+ * @separator: delimiter
+ * @n: amount of arguments in list
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (n > 0)
+	{
+		va_start(valist, n);
+
+		for (i = 1; i <= n; i++)
+		{
+			printf("%d", va_arg(valist, int));
+
+			if (i != n && separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+		va_end(valist);
+	}
+	printf("\n");
+}
