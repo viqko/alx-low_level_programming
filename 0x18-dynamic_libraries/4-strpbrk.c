888dc8e8932cd6c4332070ce1ec79bdeb9018431
TETTEH VICTOR TETTEY <tvictortetteh@gmail.com>
2023-02-08 08:26:23 -0800

    updates

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strpbrk - return pointer to byte in s that matches a byte in accept
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				s = &s[i];
+				return (s);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
